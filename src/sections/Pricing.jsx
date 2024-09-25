aacbb7eac4416ec388bb80df858be1e707f2ed71
Anish Aryal <anisharyal058@gmail.com>
2024-09-25 19:55:24 +0545

    background for pricing

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -8,18 +8,22 @@ const Pricing = () => {
     <section>
       <Element name="pricing">
         <div className="container">
-          <div className="max-w-960 pricing-head_before relative mx-auto
+          <div
+            className="max-w-960 pricing-head_before relative mx-auto
           border-l border-r border-s2 bg-s1/50 pb-40 pt-28 max-xl:max-w-4xl
-          max-lg:border-none max-md:pb-32 mx-md:pt-16">
+          max-lg:border-none max-md:pb-32 mx-md:pt-16"
+          >
             <h3
               className="h3 max-lg:h4 max-md:h5 z-3 relative max-auto
             max-14 max-w-lg text-center text-p4 max-md:mb-11 max-sm:max-w-sm"
             >
               Flexible pricing for teams of all sizes
             </h3>
-            <div className="relative z-4 mx-auto flex w-[375px] rounded-3xl
+            <div
+              className="relative z-4 mx-auto flex w-[375px] rounded-3xl
             border-[3px] border-s4/25 bg-s1/50 p-2 backdrop-blur-[6px]
-            max-md:w-[310px]">
+            max-md:w-[310px]"
+            >
               <button
                 className={clsx("pricing-head_btn", monthly && "text-p4")}
                 onClick={() => setMonthly(true)}
@@ -33,9 +37,34 @@ const Pricing = () => {
                 Annual
               </button>
 
-              <div className={clsx("g4 rounded-14 before:h-100 pricing-head_btn_before absolute left-2 top-2 h-[calc(100%-16px)] w-[calc(50%-8px)] overflow-hidden shadow-400 transition-transform duration-500", !monthly && "translate-x-full")}/>
+              <div
+                className={clsx(
+                  `g4 rounded-14 before:h-100 pricing-head_btn_before absolute
+                  left-2 top-2 h-[calc(100%-16px)] w-[calc(50%-8px)]
+                  overflow-hidden shadow-400 transition-transform duration-500`,
+                  !monthly && "translate-x-full"
+                )}
+              />
+            </div>
+            <div className="pricing-bg">
+              <img
+                src="/images/bg-outlines.svg"
+                alt="outline"
+                width={960}
+                height={380}
+                className="relative z-2"
+              />
+               <img
+                src="/images/bg-outlines-fill.png"
+                alt="outline"
+                width={960}
+                height={380}
+                className="absolute inset-0 opacity-5 mix-blend-soft-light z-2"
+              />
             </div>
           </div>
+
+          {/* pricing */}
         </div>
       </Element>
     </section>
